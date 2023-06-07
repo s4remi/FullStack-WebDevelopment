0c950f6e3843840f11a56e732e7936e4fdbb0577
s4remi <s4remi@gmail.com>
2023-06-07 12:49:47 -0700

    adding sticker for winner

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,11 +1,18 @@
-var randonNumber1 = Math.floor(Math.random() * 6);
+var randonNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randonNumber1);
 var pic = "images/dice" + randonNumber1 + ".png";
 console.log(pic);
 document.querySelector("img").setAttribute("src", pic);
 /** second player's dice**/
-var randonNumber2 = Math.floor(Math.random() * 6);
+var randonNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randonNumber2);
 var pic2 = "images/dice" + randonNumber2 + ".png";
 console.log(pic2);
 document.querySelector(".img2").setAttribute("src", pic2);
+if (randonNumber1 > randonNumber2) {
+  document.querySelector("h1").innerHTML = "🏆Player 1 Wins";
+} else if (randonNumber1 < randonNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!🏆";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
