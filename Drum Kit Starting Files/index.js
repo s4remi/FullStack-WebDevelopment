8c22b517ba4271e1633a8fe5185efb32adc945f5
s4remi <s4remi@gmail.com>
2023-06-07 13:31:44 -0700

    using different ways to select all buttons in the webpage

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,29 @@
+/*
+// FOR selecting the first button, using standard addEventListener
+document.querySelector("button").addEventListener("click", handleClick);
+function handleClick() {
+  alert("i got clicked!");
+}
+// FOR selecting the first button, using better way to use addEventListener
+// in here using anonymous function style
+document.queryselector("button").addEventListener("click", function () {
+  alert("i got clicked!");
+});
+*/
+// FOR SELECTING ALL BUTTONS V1
+const buttons = document.querySelectorAll(".drum");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
+
+function handleClick() {
+  alert("i got clicked!");
+}
+// FOR SELECTING ALL BUTTONS V2
+/*
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("meow");
+  });
+}
+*/
