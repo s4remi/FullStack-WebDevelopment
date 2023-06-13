c5f07b778bb751023d885eabbf6acc23702a8e19
s4remi <s4remi@gmail.com>
2023-06-13 10:53:48 -0700

    adding Event listeners with Jquery and js

--- jQuery/index.js
+++ jQuery/index.js
@@ -33,3 +33,43 @@ $("button").html("<em><strong>Hello<strong></em>");
 
 //changing attributes with jQuery
 $("a").attr("href", "https:www.google.com");
+
+//adding event listener with Jquery
+
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+//add event listern to all buttons
+$("button").click(function () {
+  $("button").css("color", "white");
+  $("button").css("background-color", "red");
+});
+//add event listern to all buttons without using Jquery
+document.querySelectorAll("button").forEach(function (button) {
+  button.addEventListener("click", function () {
+    //button.style.color = "white";
+    console.log(document.querySelectorAll("button").length);
+  });
+});
+
+//or:
+for (var i = 0; i < document.querySelectorAll("button").length; i++) {
+  console.log("here" + i);
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    document.querySelector("h1").style.color = "black";
+  });
+}
+//----------------
+$("input").keydown(function (evet) {
+  console.log("key pressed: " + evet.key);
+});
+$(document).keydown(function (event) {
+  $("h1").text(event.key);
+});
+
+// using "on" function for mouseover
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "white");
+});
