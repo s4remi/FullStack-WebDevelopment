0e1fc7c8b82c1c00864b5241c547f7ba96f080a8
s4remi <s4remi@gmail.com>
2023-06-20 14:45:16 -0700

    starting the game by pressing any keyboard

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,14 +2,28 @@ console.log("hi");
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
 //Use jQuery to detect when any of the buttons are clicked and trigger a handler function called ali
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   console.log(userChosenColour);
   console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
+$(document).on("keydown", function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 function nextSequence() {
+  level++;
+  $("#level-title").text("level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   console.log(randomChosenColour);
@@ -17,10 +31,20 @@ function nextSequence() {
   $("#" + randomChosenColour)
     .fadeOut(100)
     .fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  playSound(randomChosenColour);
   var id = "#" + randomChosenColour;
   console.log(id);
 }
-
-nextSequence();
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+function animatePress(currentColour) {
+  $("#" + currentColour)
+    .addClass("pressed")
+    .fadeOut(100)
+    .fadeIn(100);
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
