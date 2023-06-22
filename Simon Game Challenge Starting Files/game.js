ff62e03a50e23ac63cf787cf3c7167155c1f9fe3
s4remi <s4remi@gmail.com>
2023-06-22 00:00:56 -0700

    fixing the js code for adding levels

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,6 +4,7 @@ var gamePattern = [];
 var userClickedPattern = [];
 var level = 0;
 var started = false;
+
 //Use jQuery to detect when any of the buttons are clicked and trigger a handler function called ali
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
@@ -13,7 +14,8 @@ $(".btn").click(function () {
   playSound(userChosenColour);
   animatePress(userChosenColour);
 });
-$(document).on("keydown", function () {
+
+$(document).keydown(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -22,8 +24,9 @@ $(document).on("keydown", function () {
 });
 
 function nextSequence() {
-  level++;
+  userClickedPattern = [];
   $("#level-title").text("level " + level);
+  level++;
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   console.log(randomChosenColour);
@@ -32,8 +35,9 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
   playSound(randomChosenColour);
-  var id = "#" + randomChosenColour;
-  console.log(id);
+  $("#pattern").css("background-color", randomChosenColour);
+  // var id = "#" + randomChosenColour;
+  // console.log(id);
 }
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
@@ -47,4 +51,21 @@ function animatePress(currentColour) {
   setTimeout(() => {
     $("#" + currentColour).removeClass("pressed");
   }, 100);
+  checkAnswer(userClickedPattern.length - 1);
+}
+function checkAnswer(currentLevel) {
+  //check answer against pattern array, return false or continue playing sequence
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("Success!");
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("Wrong!");
+    userClickedPattern = [];
+    gamePattern = [];
+    $("#level-title").text("Press A Key to Start");
+  }
 }
