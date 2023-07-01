42451eca48c608d122a6d788e217453f7c76dfb5
s4remi <s4remi@gmail.com>
2023-06-30 17:16:51 -0700

    adding a package that gives us the silly  name and using it by printing it

--- p2.3 NPM/index.js
+++ p2.3 NPM/index.js
@@ -1,2 +1,9 @@
 console.log("hello i am here!");
 console.log("welcome to NPM: node package manager");
+
+// generates a random name from the list of adjectives and nouns in 'node_modules
+var generateName = require("sillyname");
+var sillyName = generateName();
+console.log("my name is" + sillyName);
+console.log("little break");
+console.log(`My name is ${sillyName}.`);
