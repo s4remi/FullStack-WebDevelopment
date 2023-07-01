04831411a18f94afad3cc2e846bf892b4a0e21dc
s4remi <s4remi@gmail.com>
2023-06-30 17:28:48 -0700

    using ESM instead of CJM for importing packages

--- p2.3 NPM/index.js
+++ p2.3 NPM/index.js
@@ -2,8 +2,17 @@ console.log("hello i am here!");
 console.log("welcome to NPM: node package manager");
 
 // generates a random name from the list of adjectives and nouns in 'node_modules
-var generateName = require("sillyname");
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+// console.log("my name is" + sillyName);
+// console.log("little break");
+// console.log(`My name is ${sillyName}.`);
+
+// using ESM instead of CJS
+import generateName from "sillyname";
 var sillyName = generateName();
-console.log("my name is" + sillyName);
-console.log("little break");
-console.log(`My name is ${sillyName}.`);
+console.log(`the new silly name is ${sillyName}`);
+var newName = generateName();
+// prints out another randomly generated name each time you run this
+console.log(`newest name is ${newName}`);
