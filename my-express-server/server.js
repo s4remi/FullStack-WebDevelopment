c8248414e75f95f38a38cdeac828499b9e64b50c
s4remi <s4remi@gmail.com>
2023-07-03 23:07:35 -0700

    adding get function that take care of the request and the response, then installing the nodemon in order to run server easier

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -7,11 +7,23 @@ const app = express();
 // app.get allows us to what should happen when a browser get in touch with our server
 // the first parameter is the location that we represent it by "/"
 // the second parameter is the callback function
-// app.get("/", function (request, response) {
-// // code to handle get requests
-//   console.log(request);
-// });
+app.get("/", function (request, response) {
+  response.send("<h1>Hi</br>; Ali!<h1>");
+  //console.log(request);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+/**installing the npm package called " nodemon",
+ * this package help us to start the server easier.
+ * for installation: make sure we are in the admin,
+ * then in the terminal npm i nodemon
+ * if that didn't work use " npm i -g nodemon"
+ * if that didn't work use " sudo npm i -g  nodemon"
+ * after installation if error occurred, get rid of them by:
+ * " npm i pstree.remy@1.1.0 -D"  
+ * (This issue is documented here:
+https://github.com/remy/nodemon/issues/1459)
+*/
