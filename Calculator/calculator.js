d809506d0681ce36d0e4ea220c35ed533bfd259b
s4remi <s4remi@gmail.com>
2023-07-04 14:06:21 -0700

    using .post method to respond back when the info recieved from the user

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -4,7 +4,7 @@ const port = 3500;
 
 app = express();
 
-//add 3500 port to listen
+//add 3000 port to listen
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
@@ -13,13 +13,18 @@ app.get("/", function (req, res) {
   res.send("welcome to the <em><span>M<span><em>ain page!");
 });
 app.get("/cal", function (req, res) {
-  res.sendFile(
-    "/Users/nobody1/Desktop/WebDev/code exercises/Calculator/index.html"
-  );
+  //hard copy the path
+  // res.sendFile(
+  //   "/Users/nobody1/Desktop/WebDev/code exercises/Calculator/index.html"
+  // );
   /** send the index.html file for the response,
    * but as we are going to upload our website on cloud we should
    * use the entire path by using  so it is easier to use __dirname" or "__filename"
    */
-  console.log(__dirname);
-  //res.sendFile(__dirname + "/index.html");
+  //console.log(__dirname);
+  //use the shortcut for the path
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function (req, res) {
+  res.send("Thanks for sending info!");
 });
