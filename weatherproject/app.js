18d0c70112f01a5c585e3886d8b3d554cb1497e4
s4remi <s4remi@gmail.com>
2023-07-06 11:27:34 -0700

    make HTTP requests in Node.js

--- weatherproject/app.js
+++ weatherproject/app.js
@@ -1,14 +1,24 @@
 console.log("Hi there!");
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-//listening port
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+//make HTTP requests in Node.js
+const https = require("https");
+const url =
+  "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw&type=single";
 
 // add get method
 app.get("/", function (req, res) {
+  // using https to get response from other api
+  https.get(url, (response) => {
+    console.log(response);
+  });
   res.send("<h1>Hello World!</h1>"); // send response to client when
+});
+
+//listening port
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
