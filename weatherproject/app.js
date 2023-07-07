da44eb05c6bdb8480e3c98111274ba8739ab86d9
s4remi <s4remi@gmail.com>
2023-07-07 10:53:56 -0700

    fixing the code for using the https for using "on" method

--- weatherproject/app.js
+++ weatherproject/app.js
@@ -4,17 +4,41 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//make HTTP requests in Node.js
+//make HTTP requests in Node.js as request module is deprecated
 const https = require("https");
+//const url ="https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw&type=single";
+
 const url =
-  "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw&type=single";
+  "https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=9b8c7b10dac45094197806527e8bc9c5&units=metric";
 
 // add get method
 app.get("/", function (req, res) {
   // using https to get response from other api
-  https.get(url, (response) => {
-    console.log(response);
-  });
+  https
+    .get(url, (response) => {
+      console.log(response);
+      console.log("\n------------\n");
+      console.log(`response.statusCode: ${response.statusCode}`);
+      console.log("\n--------\n");
+      console.log(`headers: \n${response.headers}`);
+
+      //using the "on" method to searching for the data
+      response.on("data", function (d) {
+        console.log("\n****  `data: ${d}` ***\n");
+        console.log(`data: ${d}`);
+        console.log(
+          "\n****   process.stdout.write(d); this is the equivalent of console.log(d) ***\n"
+        );
+        process.stdout.write(d);
+        console.log("\n**** JSON.parse(d) ****\n");
+        // make it readable for us
+        const weatherData = JSON.parse(d);
+        console.log(weatherData);
+      });
+    })
+    .on("error", (e) => {
+      console.log(`e:\t${e}`);
+    });
   res.send("<h1>Hello World!</h1>"); // send response to client when
 });
 
