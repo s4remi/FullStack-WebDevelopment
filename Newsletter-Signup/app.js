13c9296ba02fa30f596345689536ec769d8fd372
s4remi <s4remi@gmail.com>
2023-07-11 11:07:18 -0700

    in order to see all css files and images we need to make them public and by using: app.use(express.static("./public")) we can have those local file upload to server

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.use(express.urlencoded({ extended: true }));
+//make the server not static
+app.use(express.static("./public")); //serve public folder as it's own
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.listen(port, function (req, res) {
