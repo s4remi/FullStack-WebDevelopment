5f17b5cc70e9678b1bace28713061395a59f64de
s4remi <s4remi@gmail.com>
2023-08-09 10:13:58 -0700

    using .ejs for sending the js result back to be in the html page

--- p4.0 EJS/index.js
+++ p4.0 EJS/index.js
@@ -5,9 +5,25 @@ const app = express();
 const port = 4500;
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World</h1>");
+  const today = new Date("2022-03-22");
+  const day = today.getDay();
+
+  console.log(day);
+  //res.send("<h1>Hello World</h1>");
+
+  let type = "a weekday";
+  let adv = "It's time to work hard!";
+  if (day === 0 || day === 6) {
+    type = "weekend";
+    adv = "Time for a break!";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`the server is running in port ${port}`);
+  console.log(`the server is running on port ${port}`);
 });
