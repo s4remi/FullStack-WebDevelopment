1ff27ab24364da8fdcfb17716664f68486fe32a0
s4remi <s4remi@gmail.com>
2023-08-14 12:17:35 -0700

    last edite

--- p4.0 EJS/index.js
+++ p4.0 EJS/index.js
@@ -1,20 +1,26 @@
-console.log("Hi we are here!");
+console.log("Hi, The start!");
 import express from "express";
 
 const app = express();
 const port = 4500;
 
 app.get("/", function (req, res) {
-  const today = new Date("2022-03-22");
+  const today = new Date("2022-03-28", "15:181");
   const day = today.getDay();
+  console.log(`the day is : ${day}`);
+  for (let i = 0; i < 7; i++) {
+    var date = new Date("2022/03/" + String(today.getDate));
+    console.log("we here!");
+    console.log(date);
+  }
 
-  console.log(day);
+  //console.log(day);
   //res.send("<h1>Hello World</h1>");
 
   let type = "a weekday";
   let adv = "It's time to work hard!";
   if (day === 0 || day === 6) {
-    type = "weekend";
+    type = "the weekend";
     adv = "Time for a break!";
   }
 
