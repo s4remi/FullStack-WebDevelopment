0ef59d79bccf28bbe5b779a3bb607b85d02fe0b6
s4remi <s4remi@gmail.com>
2023-08-28 10:15:30 -0700

    counting the word's digits

--- p4.2 Passing Data/index.js
+++ p4.2 Passing Data/index.js
@@ -2,13 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = 3;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  let first = "name";
+  let last = "last";
+  res.render("index.ejs", { fn: first, ln: last });
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const name = req.body.fName;
+  const surname = req.body.lName;
+  console.log(`the first name is ${name} and the last name is ${surname}`);
+  let count = name.length + surname.length;
+  console.log(`the count is ${count}`);
+  res.send(
+    `<h1><em><strong>the<strong> count for the word you type is<em> ${count}`
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
